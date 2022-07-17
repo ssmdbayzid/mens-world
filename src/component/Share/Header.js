5f3b47035129e4731cdf220f31551300e76bc9a4
ssmdbayzid <ssmd.bayzid@gmail.com>
2022-07-17 19:00:59 +0600

    set header

--- src/component/Share/Header.js
+++ src/component/Share/Header.js
@@ -19,18 +19,18 @@ const Header = () => {
     return (
         <div className='bg-green-100 shadow'>
             <nav className=' container mx-auto md:flex md:items-center md:justify-between'>
-                <div className='justify-between'>
-                    <div >
-                        <a href='/' className='flex items-center w-full py-1'>
-                        <img src={logo} className="w-[75px]" alt='' />
-                        <h1 className='p-2 text-2xl w-[200px] font-[600]'>Mens World</h1>
+                <div className='flex items-center justify-between'>
+                    <div className='flex md:block'>
+                        <a href='/' className='md:flex text-center items-center py-4 md:py-2'>
+                        <img src={logo} className=" w-[50px] md:w-[75px] mx-auto" alt='' />
+                        <h1 className='md:p-2 pl-2 text-lg w-[100px]  md:text-2xl md:w-[150px] md:font-[600] font-[300]'>Mens World</h1>
                         </a>
                     </div>
                     <div onClick={()=>setOpen(!open)} className={`w-12 text-4xl md:hidden`}>
                         {open? < CgClose /> : <BiMenu /> }
                     </div>
                 </div>
-                <ul className={`md:flex text-center w-full bg-cyne-100 justify-center md:sticky absolute duration-1000 ease-in-out ${open ? 'top-6': 'top-[-450px]'}`}>
+                <ul className={`md:flex mx-auto text-center bg-cyne-100 justify-center md:sticky absolute duration-1000 ease-in-out ${open ? 'top-6': 'top-[-450px]'}`}>
                     {/* <li className='mx-2 hover:text-cyan-700 font-semibold'><a href='/#'>Home</a></li>
                     <li className='mx-2 hover:text-cyan-700 font-semibold'><a href='/#'>Products</a></li>
                     <li className='mx-2 hover:text-cyan-700 font-semibold'><a href='/#'>Log In</a></li>
